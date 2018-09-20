6d9607abf3b74b23d3285cf11aadb918539c7abb
aindreboe <aindreboe@gmail.com>
2018-09-20 10:35:50 +0100

    Adding classes to be implemented

---
+++ LoyaltyCard.java
@@ -0,0 +1,44 @@
+package impl;
+
+import common.InsufficientPointsException;
+import interfaces.ILoyaltyCard;
+import interfaces.ILoyaltyCardOwner;
+
+/**
+ * This class represents a Loyalty Card, recording information relating to an owners use of the card.
+ *
+ */
+public class LoyaltyCard implements ILoyaltyCard {
+
+    private interfaces.ILoyaltyCardOwner owner;
+
+    @Override
+    public ILoyaltyCardOwner getOwner() {
+        // TODO Auto-generated method stub
+        return owner;
+        return null;
+    }
+
+    @Override
+    public int getNumberOfUses() {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    public int getNumberOfPoints() {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    public void addPoints(int points) {
+        // TODO Auto-generated method stub
+    }
+
+    @Override
+    public void usePoints(int points) throws InsufficientPointsException {
+        // TODO Auto-generated method stub
+    }
+
+}
---
+++ W03JUnit.java
@@ -0,0 +1,9 @@
+package impl;
+
+public class W03JUnit {
+    public static void main(String[] args) {
+
+
+
+    }
+}
